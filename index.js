598ebb48dfbb06c80b48d60b20ad94670c4f1ed6
Soham Ghodke <sohamghodke.2006@gmail.com>
2026-06-28 22:17:09 +0530

    New Commit

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.urlencoded({ extended : true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.static("public"));
+app.set(express.static(path.join(__dirname, "public")));
+
+
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
